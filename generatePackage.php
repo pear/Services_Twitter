2620d6522d21badd80b0ae4c397a9aca8123bad0
Bill Shupp <shupp@php.net>
2010-06-01 01:19:00 +0000

    * Added generatePackage.php for easy package.xml management * Bumped version to 0.6.0 for trends/lists release
    git-svn-id: http://svn.php.net/repository/pear/packages/Services_Twitter/branches/lists_trends@300017 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ generatePackage.php
@@ -0,0 +1,75 @@
+<?php
+
+require_once('PEAR/PackageFileManager2.php');
+
+PEAR::setErrorHandling(PEAR_ERROR_DIE);
+
+$packagexml = new PEAR_PackageFileManager2;
+
+$packagexml->setOptions(array(
+    'baseinstalldir'    => '/',
+    'simpleoutput'      => true,
+    'packagedirectory'  => './',
+    'filelistgenerator' => 'file',
+    'ignore'            => array('runTests.php', 'generatePackage.php'),
+    'dir_roles' => array(
+        'tests'     => 'test',
+        'data'      => 'data',
+        'examples'  => 'doc'
+    ),
+));
+
+$packagexml->setPackage('Services_Twitter');
+$packagexml->setSummary('PHP interface to Twitter\'s API');
+$packagexml->setDescription(
+    'An interface for communicating with Twitter\'s public API. Send status updates, fetch information, add friends, etc.'
+);
+
+$packagexml->setChannel('pear.php.net');
+$packagexml->setAPIVersion('0.6.0');
+$packagexml->setReleaseVersion('0.6.0');
+
+$packagexml->setReleaseStability('beta');
+
+$packagexml->setAPIStability('beta');
+
+$packagexml->setNotes('
+* Added lists and trends support from http://blog.cheki.net/archives/1618
+* Added generatePackage.php for easy package.xml management
+');
+$packagexml->setPackageType('php');
+$packagexml->addRelease();
+
+$packagexml->detectDependencies();
+
+$packagexml->addMaintainer('lead',
+                           'shupp',
+                           'Bill Shupp',
+                           'shupp@php.net');
+
+$packagexml->addMaintainer('lead',
+                           'izi',
+                           'David Jean Louis',
+                           'izi@php.net');
+
+$packagexml->addMaintainer('lead',
+                           'jstump',
+                           'Joe Stump',
+                           'joe@joestump.net');
+
+$packagexml->setLicense('New BSD License',
+                        'http://www.opensource.org/licenses/bsd-license.php');
+
+$packagexml->setPhpDep('5.2.0');
+$packagexml->setPearinstallerDep('1.4.0b1');
+$packagexml->addPackageDepWithChannel('required', 'PEAR', 'pear.php.net', '1.4.0');
+$packagexml->addPackageDepWithChannel('required', 'HTTP_Request2', 'pear.php.net');
+$packagexml->addPackageDepWithChannel('optional', 'HTTP_OAuth', 'pear.php.net', '0.1.7');
+$packagexml->addExtensionDep('required', 'json'); 
+$packagexml->addExtensionDep('required', 'mbstring'); 
+$packagexml->addExtensionDep('required', 'simplexml'); 
+
+$packagexml->generateContents();
+$packagexml->writePackageFile();
+
+?>
